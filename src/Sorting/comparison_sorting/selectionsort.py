74b072a3d8374ef5fdf144e67e9a1d62e08cae37
arhaanarif <arhaanarifsaifi@gmail.com>
2024-10-21 10:58:05 +0530

    selection sort

---
+++ src/Sorting/comparison_sorting/selectionsort.py
@@ -0,0 +1,16 @@
+def selectionSort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+arr = [5, 3, 2, 4, 1]
+print(selectionSort(arr))
+
+
+
